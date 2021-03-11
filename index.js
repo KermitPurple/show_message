f0b4316b7389fae65c6c297677b278e988cf728b
Shane McDonough <shanemcdman@gmail.com>
2021-03-10 21:27:16 -0800

    set title and dcode uri component

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ let GET = {};
 for(let substr of document.location.search.split(/\?|&/))
     if(substr){
         let [key, val] = substr.split('=');
-        document.querySelector(key).innerHTML = val;
+        document.querySelector(key).innerHTML = decodeURIComponent(val);
     }
+//set title
+document.title = document.querySelector('h1').innerHTML;
